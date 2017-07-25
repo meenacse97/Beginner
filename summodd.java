bbe5ce3d2f84faf700e1ff36d863f11c46738c86
meenacseengineering5 <meenacse1997@gmail.com>
2017-07-25 10:15:47 +0530

    Add files via upload

--- summodd.java
+++ summodd.java
@@ -1,5 +1,19 @@
 package guvi;
-
 public class summodd {
+	public static void main(String[] args) {
+		int sum1=0;
+		int sum2=0;
+		for(int i=1;i<=15;i++){
+			sum1=sum1+i;
+		}
+		System.out.println(sum1);
+		for(int i=15;i<=45;i++){
+			if(i%2!=0){
+				sum2=sum2+i;
+			}
+		}
+		System.out.println(sum2);
+
+	}
 
 }
